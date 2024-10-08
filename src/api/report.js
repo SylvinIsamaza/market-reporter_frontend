57486798129260b60e6bb6b5425fd466f1114a53
tox-in <habimanat78@gmail.com>
2024-10-08 20:42:38 +0200

    fetchReports by user id

--- src/api/report.js
+++ src/api/report.js
@@ -1,26 +1,32 @@
-import toast from "react-hot-toast"
-import client from "./client"
+import toast from "react-hot-toast";
+import client from "./client";
+
 export const addReport = async (propertyData) => {
+  const responsePromise = client.post("/report", propertyData, { withCredentials: true });
 
-  
+  toast.promise(responsePromise, {
+    loading: "Generating PDF...",
+    success: "Report generated successfully!",
+    error: (error) => error.response?.data?.message || "Error generating report.",
+  });
+
+  return (await responsePromise).data;
+};
+
+
+export const fetchReports = async (month, year) => {
   try {
-    const response = client.post("/report", propertyData,{withCredentials:true})
-    toast.promise(
-      response,
-      {
-
-      loading: "Generating  pdf",
-      success: "Successfully generated",
-       
-      error: (error) => {
-       return error.response.data.message||"Error generating report"
-      }
-      })
+    const response = await client.get(`/report`, {
+      params: { month, year },
+      withCredentials: true,
+    });
     
-    return (await response).data
+    return response.data || [];
     
   } catch (error) {
-    throw error
-  
+    console.error("Error fetching reports:", error);
+    toast.error(error.response?.data?.message || 'Failed to fetch reports');
+    
+    throw new Error(error.response?.data?.message || 'Failed to fetch reports');
   }
-}
+};
